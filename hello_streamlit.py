82badc4a43bbf9d7c97b76bea8ce0a45933f1c9e
xid <daemonxid@gmail.com>
2025-08-12 10:34:38 +0900

    streamlit 배포 및 커밋 연습

---
+++ hello_streamlit.py
@@ -0,0 +1,61 @@
+# hello_streamlit.py
+import streamlit as st
+
+# 제목 추가
+st.title("🎉 내 첫 번째 Streamlit 앱!")
+
+# 텍스트 추가
+st.write("안녕하세요! Streamlit으로 만든 웹 애플리케이션입니다.")
+
+# 버튼 추가
+if st.button("클릭해보세요!"):
+    st.success("버튼이 클릭되었습니다! 🎊")
+
+# 실행방법
+# python hello_streamlit.py = x
+# streamlit run hello_streamlit.py
+st.badge("New")
+st.badge("Success", icon=":material/check:", color="green")
+
+st.markdown(
+    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
+)
+import pandas as pd
+import pydeck as pdk
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(
+    rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+
+st.pydeck_chart(
+    pdk.Deck(
+        map_style=None,  # Use Streamlit theme to pick map style
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=df,
+                get_position="[lon, lat]",
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=df,
+                get_position="[lon, lat]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=200,
+            ),
+        ],
+    )
+)
